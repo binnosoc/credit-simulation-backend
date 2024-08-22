a6ec57c73f6b6397f7c4c75515d67e8bdf527abf
Boro Ousseni <bin.innosoc@gmail.com>
2024-08-22 14:01:53 +0100

    first commit

---
+++ src/test/java/com/binnosoc/simulation/SimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binnosoc.simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
